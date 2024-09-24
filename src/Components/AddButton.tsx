fedef82a51a1f5f4d89601149791ceaa544b8412
Josemario <97921175+Josemario17@users.noreply.github.com>
2024-09-24 18:43:38 +0100

    feat: Base do Projecto

---
+++ src/Components/AddButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AddButton({ text, typeButton, handleClick }: { text: string, typeButton: string, handleClick: any }){
+    return (
+        <>
+            <button onClick={handleClick} type={typeButton === "Submit" ? "submit" : "button"} className="text-white inline-flex my-3 items-center bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <svg className="h-3.5 w-3.5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                </svg>
+                {text}
+            </button>
+        </>
+    )
+}
---
+++ src/Components/Table.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+export default function TableList({ EditModalState, DeleteModalState } : { EditModalState : any , DeleteModalState : any} ) {
+    return (
+        <>
+            <div className="overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            {
+                                Array("Nome", "Email", "Cargo").map((item) => (<th scope="col" className="px-4 py-4">{item}</th>))
+                            }
+                            <th scope="col" className="px-4 py-3">
+                                <span className="sr-only">Actions</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b dark:border-gray-700">
+                            <td className="px-4 py-3 text-white">José Mário</td>
+                            <td className="px-4 py-3 text-white">Josydossantos2002@gmail.com</td>
+                            <th scope="row" className="px-4 py-3 font-medium whitespace-nowrap text-white">Desenvolvedor</th>
+                            <td className="px-4 py-3 flex items-center justify-end">
+                                <div className="py-1 text-sm flex gap-1">
+                                    <button onClick={ ()=> EditModalState(true) } type="button" className="flex w-full items-center py-2 px-4 hover:scale-110 duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </button>
+                                    <button onClick={ ()=> DeleteModalState(true) }  type="button" className="flex w-full items-center py-2 px-4 text-red-500 hover:scale-110 duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <nav className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 space-x-4 gap-12 md:space-y-0 p-4">
+                <span className="text-sm font-normal gap-2 flex text-gray-500 dark:text-gray-400">
+                    Mostrando
+                    <span className="font-semibold text-gray-900 dark:text-white">1-10</span>
+                    de
+                    <span className="font-semibold text-gray-900 dark:text-white">1000</span>
+                </span>
+                <div className="inline-flex items-stretch -space-x-px">
+                    <button className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <span className="sr-only">Anterior</span>
+                        <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    <button className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <span className="sr-only">Avançar</span>
+                        <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </nav>
+        </>
+    )
+}
